87b05c7fb3125620fec593a8ffe237007963fd3c
Viraat Aryabumi Reddy <viraat94@gmail.com>
2015-04-07 11:57:14 +0530

    updates

--- AutoSuggest/main.c
+++ AutoSuggest/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ALPHABET_SIZE = 26;
+
+typedef struct node
+{
+    bool isEndOfWord;
+    struct node* next[ALPHABET_SIZE];
+}node1;
+
+Node *createNode()
+{
+    
+}
+int showMenu()
+{
+    int choice;
+    printf("\nMenu\n----\n");
+    printf("1. Insert word\n2. Auto Suggest Word\n3. Print All Words\n4. Exit\n\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    return choice;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    int choice;
+    while(1)
+    {
+        choice  = showMenu();
+        switch (choice)
+        {
+        case 4:
+            return 0;
+        default:
+            printf("\nBad choice...");
+        }
+    }
     return 0;
 }
+
