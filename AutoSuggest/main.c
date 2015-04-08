b197850e5fddfaacc8c5c5c73ed079c1954b05d0
Viraat Aryabumi Reddy <viraat94@gmail.com>
2015-04-08 19:40:50 +0530

    finished project

--- AutoSuggest/main.c
+++ AutoSuggest/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define ALPHA_SIZE 26
+#include <string.h>
 
-typedef struct node //simplified linked list to implement the word data structure
+#define ALPHA_SIZE 26
+typedef struct n
 {
     bool isEOW;
-    struct node *next[ALPHA_SIZE];
+    struct n *next[ALPHA_SIZE];
 }Node;
 
-Node *createNode() //function to init the node
+Node *CreateNode()
 {
     int i;
     Node *newNode = malloc(sizeof(Node));
     newNode->isEOW = false;
-    for(i=0; i<ALPHA_SIZE; i++)
+    for(i=0;i<ALPHA_SIZE;i++)
         newNode->next[i] = NULL;
     return newNode;
 }
@@ -23,42 +24,102 @@ void insertWord(Node *root, char word[])
 {
     int i, idx;
     Node *tmp = root;
-    for(i=0; word[i]!='\0';i++)
+    for(i=0;word[i]!='\0';i++)
     {
-        idx = word[i] - 'a'; //to get the array index of alphabet in word[i]
-        if(tmp->next[idx] == NULL) //if char points to nothing
-            tmp->next[idx] = createNode(); //init the node
-        tmp = tmp->next[idx]; //move tmp to the next one
+        idx = word[i] - 'a';
+        if(tmp->next[idx] == NULL)
+            tmp->next[idx] = CreateNode();
+        tmp = tmp->next[idx];
     }
-    tmp->isEOW = true; //word ends, set isEOW to true
-    printf("\nInserted %s", word);
-    //return true;
+    tmp->isEOW = true;
+    //printf("\nInserted %s", word);
 }
 
-int showMenu() //function to show the user the menu
+void PrintAllWords(Node *root, char prefix[])
+{
+    if(root->isEOW == true)
+        printf("\n%s", prefix);
+
+    int i;
+    for(i=0;i<ALPHA_SIZE;i++)
+    {
+        if(root->next[i] != NULL)
+        {
+            int len = strlen(prefix);
+            char *newPrefix = malloc(len+2);
+            strcpy(newPrefix, prefix);
+            newPrefix[len] = i+'a';
+            newPrefix[len+1] = '\0';
+            PrintAllWords(root->next[i],newPrefix);
+            free(newPrefix);
+        }
+    }
+}
+
+void AutoSuggest(Node *root, char prefix[])
+{
+    int i, idx;
+    for(i=0;prefix[i]!='\0';i++)
+    {
+        idx = prefix[i] - 'a';
+        if(root->next[idx] == NULL)
+        {
+            printf("\nNo words starting with prefix");
+            return;
+        }
+        root = root->next[idx];
+    }
+    PrintAllWords(root, prefix);
+}
+
+int showMenu()
 {
     int choice;
-    printf("\nMenu\n----\n");
-    printf("1. Insert word\n2. Auto Suggest Word\n3. Print All Words\n4. Exit\n\n");
-    printf("Enter your choice: ");
+    printf("\nMENU");
+    printf("\n1. Insert Word");
+    printf("\n2. AutoSuggest Word");
+    printf("\n3. Print All Words");
+    printf("\n4. EXIT");
+    printf("\nYour Choice: ");
     scanf("%d", &choice);
     return choice;
 }
 
 int main()
 {
-    int choice;
-    Node *root = createNode();
+    int ch;
     char word[100];
+    Node *root = CreateNode();
+    FILE *fp = fopen("/Users/var/Downloads/AutoSuggest/dictionary.txt", "r");
+    if(fp == NULL)
+    {
+        printf("\nError opening file.");
+        return -1;
+    }
+    while(!feof(fp))
+    {
+        fscanf(fp, "%s", word);
+        insertWord(root, word);
+    }
+    fclose(fp);
+
     while(1)
     {
-        choice  = showMenu();
-        switch (choice)
+        ch = showMenu();
+        switch(ch)
         {
         case 1:
             printf("\nEnter word: ");
-            scanf("%s", word); //array name points to first element of array
+            scanf("%s", word);
             insertWord(root, word);
+            break;
+        case 2:
+            printf("\nEnter prefix: ");
+            scanf("%s", word);
+            AutoSuggest(root, word);
+            break;
+        case 3:
+            PrintAllWords(root, "");
             break;
         case 4:
             return 0;
@@ -66,5 +127,6 @@ int main()
             printf("\nBad choice...");
         }
     }
+
     return 0;
 }
